cf74df298d077f715a6302d37ff7a92afa3abb76
huynh23trh <tranghttpc02468@fpt.edu.vn>
2024-01-11 18:12:32 +0700

    gitignore is now working

---
+++ PC02468/src/Model/BaseModel.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Model;
+
+class BaseModel{
+    public function __construct(){
+        echo '<br> file Base Model <br>';
+    }
+}
---
+++ PC02468/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php2/baitap',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '96bf0d1d4d4d69b934b31c212e16a6268eaffcee',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php2/baitap' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '96bf0d1d4d4d69b934b31c212e16a6268eaffcee',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
